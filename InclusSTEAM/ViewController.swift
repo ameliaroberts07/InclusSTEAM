47b3c69aff4ad1e50f740b09fdd7a17782bfbbc1
ameliaroberts07 <ameliaroberts101107@gmail.com>
2022-06-14 14:02:11 -0400

    Initial Commit

---
+++ InclusSTEAM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InclusSTEAM
+//
+//  Created by Starr Price on 6/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
