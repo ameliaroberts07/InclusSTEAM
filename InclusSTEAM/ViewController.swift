40500ba3f108a2ac81671e17759fe3368fd104b2
ameliaroberts07 <ameliaroberts101107@gmail.com>
2022-06-15 11:14:35 -0400

    ADDED CODE AND ADDED FIGURES IN STEM PAGE AND SEGUES

--- InclusSTEAM/ViewController.swift
+++ InclusSTEAM/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ InclusSTEAM/ViewController2.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController2.swift
+//  InclusSTEAM
+//
+//  Created by Starr Price on 6/15/22.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+        
+        }
+    
+}
